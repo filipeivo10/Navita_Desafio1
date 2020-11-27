411e5327e11f86b6886bdae0edd401512229d104
Lucas Pagliarini <53023632+lucaspag89@users.noreply.github.com>
2020-11-27 01:41:46 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Desafio1.Services;
+using System;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolutionService.Solution(585);
+            Console.WriteLine(SolutionService.Solution(678));
+        }
+    }
+}
---
+++ Services/SolutionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio1.Services
+{
+    public class SolutionService
+    {
+        public static uint Solution(uint N)
+        {
+
+
+            return N;
+        }
+    }
+}
